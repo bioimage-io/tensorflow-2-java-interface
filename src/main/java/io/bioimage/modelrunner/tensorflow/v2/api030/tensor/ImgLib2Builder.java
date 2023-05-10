1f5f74847cb64f7fdcc1f1ba448c2ad59bcfdd93
carlosuc3m <100329787@alumnos.uc3m.es>
2023-05-10 22:24:26 +0200

    update license blurbs

--- src/main/java/io/bioimage/modelrunner/tensorflow/v2/api030/tensor/ImgLib2Builder.java
+++ src/main/java/io/bioimage/modelrunner/tensorflow/v2/api030/tensor/ImgLib2Builder.java
@@ -1,7 +1,7 @@
 /*-
  * #%L
  * This project complements the DL-model runner acting as the engine that works loading models 
- * 	and making inference with Java 0.3.0 and newer APIs for Tensorflow 2.
+ * 	and making inference with Java 0.3.0 and newer API for Tensorflow 2.
  * %%
  * Copyright (C) 2022 - 2023 Institut Pasteur and BioImage.IO developers.
  * %%

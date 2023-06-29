346daf6b035cd9a105515469108967e20c4517f0
carlosuc3m <100329787@alumnos.uc3m.es>
2023-06-29 18:50:48 +0200

    improve process destruction

--- src/main/java/io/bioimage/modelrunner/tensorflow/v2/api030/Tensorflow2Interface.java
+++ src/main/java/io/bioimage/modelrunner/tensorflow/v2/api030/Tensorflow2Interface.java
@@ -167,6 +167,10 @@ public class Tensorflow2Interface implements DeepLearningEngineInterface {
      * HashMap that maps tensor to the temporal file name for interprocessing
      */
     private HashMap<String, String> tensorFilenameMap;
+    /**
+     * Process where the model is being loaded and executed
+     */
+    Process process;
     
     /**
      * TODO the interprocessing is executed for every OS
@@ -294,7 +298,7 @@ public class Tensorflow2Interface implements DeepLearningEngineInterface {
 			for (Tensor tensor : inputTensors) {args.add(getFilename4Tensor(tensor.getName()) + INPUT_FILE_TERMINATION);}
 			for (Tensor tensor : outputTensors) {args.add(getFilename4Tensor(tensor.getName()) + OUTPUT_FILE_TERMINATION);}
 			ProcessBuilder builder = new ProcessBuilder(args);
-	        Process process = builder.start();
+	        process = builder.start();
 	        int result = process.waitFor();
 	        process.destroy();
 	        if (result != 0)
@@ -355,6 +359,8 @@ public class Tensorflow2Interface implements DeepLearningEngineInterface {
 				ff.delete();
 		}
 		listTempFiles = null;
+		if (process != null)
+			process.destroyForcibly();
 	}
 
 	// TODO make only one
